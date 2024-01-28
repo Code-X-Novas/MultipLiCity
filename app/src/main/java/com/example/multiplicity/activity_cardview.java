4925065663e612cb3e3a258e1e2b6b076e71a72f
uraharaSky <akashbiswaliit7@gmail.com>
2024-01-28 08:11:18 +0530

    XML pages created Splash Screen created Pages created till activity_login_page.xml ID created

--- app/src/main/java/com/example/multiplicity/activity_cardview.java
+++ app/src/main/java/com/example/multiplicity/activity_cardview.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class bt_nav extends AppCompatActivity {
+public class activity_cardview extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_bt_nav);
+        setContentView(R.layout.activity_cardview);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
